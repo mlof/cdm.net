31b4b9d7fd1e397ee4a3302f8bde1fafebfb41fc
Marcel Lof <marcelrlof@gmail.com>
2019-04-03 08:55:43 +0200

    Added Generator project

---
+++ src/cdm.net.generator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NJsonSchema;
+namespace cdm.net.generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
